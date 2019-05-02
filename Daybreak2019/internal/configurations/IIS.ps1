cc4af438332224da32f4a9cb258276bc86044faa
Eric Boersma <boersma.eric.j@gmail.com>
2019-05-02 15:14:53 -0700

    stuff

--- Daybreak2019/internal/configurations/IIS.ps1
+++ Daybreak2019/internal/configurations/IIS.ps1
@@ -16,9 +16,44 @@ Configuration IIS {
         xWebsite DefaultSite    {
             Ensure          = 'Present'
             Name            = 'Default Web Site'
+            State           = 'Started'
+            PhysicalPath    = 'C:\inetpub\wwwroot'
+            DependsOn       = '[WindowsFeature]IIS'
+        }
+
+        WindowsFeature NLB
+        {
+            Ensure = "Present"             
+            Name = "NLB"
+        }
+
+        WindowsFeature WID
+        {
+            Ensure = "Present"             
+            Name = "Windows-Internal-Database"
+        }       
+
+        WindowsFeature PowerShell2
+        {
+            Ensure = "Absent"             
+            Name = "PowerShell-V2"
+        }           
+        
+        xWebsite DefaultSite    {
+            Ensure          = 'Present'
+            Name            = 'Default Web Site'
             State           = 'Stopped'
             PhysicalPath    = 'C:\inetpub\wwwroot'
             DependsOn       = '[WindowsFeature]IIS'
         }
+       
+        WindowsEventLog ApplicationEventlogSize
+        {
+            LogName            = 'Application'
+            IsEnabled          = $true
+            LogMode            = 'Circular'
+            MaximumSizeInBytes = 4096000KB
+        } # End of Windows Event Log Resource
+
     }
 }
